d8e0543d7d1a8c28903aa11a351a1d03e964088a
pywugate <62863435+pywugate@users.noreply.github.com>
2021-07-26 11:40:36 +0800

    Add files via upload

--- ExecuteSingle.m
+++ ExecuteSingle.m
@@ -1,24 +1,23 @@
-%
-%%%%%%%%%%%%%%%%%%%         Selection        %%%%%%%%%%%%%%%%%%%
 tic;
+%%%%%%%%%%%%%%%%%%%           Selection         %%%%%%%%%%%%%%%%%%%
 [filename, path] = uigetfile('*.ome.tiff', 'Choose a file to open');
-%
-%%%%%%%%%%%%%%%%%%% Question: Invert colour? %%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%%   Question: Invert colour?  %%%%%%%%%%%%%%%%%%%
 group = 'Updates';
 pref =  'ColourInversion';
 title = 'Inverting';
 quest = {'Do you want to invert colour?'...
-    'e.g. White background with Black data,  BUT you want the opposite  '...
+    'e.g. White background with Black data, BUT you want the opposite  '...
     'The decision is for all the files in same foler'};
 pbtns = {'Yes','No'};
 [pval,tf] = uigetpref(group,pref,title,quest,pbtns);
-%
-%%%%%%%%%%%%%%%%%%%     Save individual Z    %%%%%%%%%%%%%%%%%%%
+
+%%%%%%%%%%%%%%%%%%% Load data, Save individual Z %%%%%%%%%%%%%%%%%%%
 switch pval
     case No
-        DetectRS_OMETIFF(filename,path);        % if you don't need inversion
+        DetectRS_OMETIFF(filename,path);
     case Yes
-        DetectRS_OMETIFF_inverC(filename,path); % if you need inversion
+        DetectRS_OMETIFF_inverC(filename,path);
 end
+%%%%%%%%%%%%%%%%%%%                              %%%%%%%%%%%%%%%%%%%
 toc;
-%
