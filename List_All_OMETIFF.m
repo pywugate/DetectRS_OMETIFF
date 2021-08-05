862560241b2ed6b7d06af9bbbe61ecf8c29043b0
pywugate <62863435+pywugate@users.noreply.github.com>
2021-08-05 14:56:40 +0800

    Add files via upload

--- List_All_OMETIFF.m
+++ List_All_OMETIFF.m
@@ -1,4 +1,4 @@
-function filelist=Listint_All_OMETIFF
+function ometifflist=List_All_OMETIFF
 
 rootdir  = uigetdir( 'select folder');
 
@@ -6,13 +6,16 @@ if isempty(dir(rootdir))
     error('Invalid folder path.')
 end
 
-s = dir (fullfile(rootdir, '**\*.ome.tiff*'));
-
 % listing all the '.ome.tiff' files
-str = {s.name}; 
-idx = contains (str, '.ome.tiff');
-filelist = s(idx);
-clear str s idx
+ometifflist = dir (fullfile(rootdir, '**\*.ome.tiff*'));
+clear rootdir;
+
+% if want to list all files and filter out the ometiff
+% s = dir (fullfile(rootdir));
+% str = {s.name}; 
+% idx = contains (str, '.ome.tiff');
+% ometifflist = s(idx);
+% clear str s idx
 
 end
 
