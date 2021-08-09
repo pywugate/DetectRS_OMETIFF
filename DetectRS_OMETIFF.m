5da75eff77c5243321967f808a917d9a483dfc00
pywugate <62863435+pywugate@users.noreply.github.com>
2021-08-09 11:47:13 +0800

    Add files via upload

--- DetectRS_OMETIFF.m
+++ DetectRS_OMETIFF.m
@@ -1,11 +1,11 @@
 tic;
 %%%%%%%%%%%%%%%%%%%            Selection          %%%%%%%%%%%%%%%%%%%
-filelist = List_All_OMETIFF;
+ometifflist = List_All_OMETIFF;
 
 %%%%%%%%%%%%%%%%%%% Detect,Load,Save individual Z %%%%%%%%%%%%%%%%%%%
-for i= 1:length(filelist)
-    filename = filelist(i).name;
-    path = strcat(filelist(i).folder, '\');
+for i= 1:length(ometifflist)
+    filename = ometifflist(i).name;
+    path = strcat(ometifflist(i).folder, '\');
 	DetectRS(filename,path);
 end
 %%%%%%%%%%%%%%%%%%%                               %%%%%%%%%%%%%%%%%%%
